863a1a05abbc3117792eae4c1f0add8b5d8cbf95
Vinicius Cardoso <vinic8670@gmail.com>
2022-06-29 15:04:37 -0300

    aula 04

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        blur: 'url(/src/assets/blur-backgroud.png)'
+      },
+      
       fontFamily: {
         sans: 'Roboto, sans-serif',
       },
